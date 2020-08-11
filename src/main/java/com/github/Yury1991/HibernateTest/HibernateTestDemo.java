50c640a53244e46e7a45d0eeec75cac68b76c96f
Yury <jurkas26@yandex.ru>
2020-08-11 17:30:23 +0300

    First commit

---
+++ src/main/java/com/github/Yury1991/HibernateTest/HibernateTestDemo.java
@@ -0,0 +1,7 @@
+package com.github.Yury1991.HibernateTest;
+
+import org.hibernate.SessionFactory;
+
+public class HibernateTestDemo {
+	SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+}
